4ed6708bfdbbe1ee9362bcca9ef1c542dd9fed0c
Ryan Tablada <ryan.tablada@gmail.com>
2016-11-16 11:09:53 -0600

    chore: Use Ember Data for findAll

---
+++ app/models/project.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  goal: DS.attr('number'),
+  description: DS.attr('string'),
+
+  pledgeLevels: DS.hasMany('pledge-level'),
+});
